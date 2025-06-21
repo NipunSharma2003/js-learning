549f55b88f5b26b1356f02900f960573c20b4e53
NipunSharma2003 <sharmanipun147@gmail.com>
2025-06-21 06:02:12 +0000

    variables details

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=1234
+console.log(accountId);
+let accountEmail="abc@gmail.com"//scope ki dikkat nhi aati hai
+password="Abcd"
+console.table([accountEmail,password,accountId])
+/* null->stanalone value assign kr skte hai object hai
+undefined*/
+console.log(typeof null)
+let score="33abc";
+console.log(typeof score);
+let isNum=Number(score);
+console.log(typeof isNum)
+console.log(isNum);
