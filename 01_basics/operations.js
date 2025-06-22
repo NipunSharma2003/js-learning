054ea22981aed2ad63d6f74d28a2a356fcaecdfa
NipunSharma2003 <sharmanipun147@gmail.com>
2025-06-22 06:42:03 +0000

    operations in js

---
+++ 01_basics/operations.js
@@ -0,0 +1,21 @@
+let name="Nipun";
+let surname="Sharma";
+let fullname=name+surname;
+console.log(fullname);
+console.log("2">1);
+console.log("2"==2);
+console.log("2"===2);//Strictly check krta hai
+const id=Symbol('123');
+const anotherid=Symbol('123');//for uniqueness
+console.log(id===anotherid);
+const heros=["a","b","c"]//arrays non primitive data types object
+let obj={
+    name:"Nipun",
+    age:21,
+}
+const myfun=function(){
+console.log("Hello");
+
+} 
+console.log(typeof heros);
+
